882033ad11e22f18bf1aa4be5342b2724c22c828
aljapri <122621599+aljapri@users.noreply.github.com>
2024-02-07 19:33:47 +0300

    file and folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 import React from "react";
 import "./App.css";
+import { CssBaseline } from "@mui/material";
+import { BrowserRouter, Navigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import { StatHeader } from "./styles";
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="movies" />} />
+          <Route path="movies" element={<StatHeader>hello</StatHeader>} />
+          <Route path="movie/:id" element={<h1>movie id</h1>} />
+          <Route path="actor/:id" element={<h1>movie id</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
---
+++ src/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const StatHeader = styled("div")(
+  () => `
+    color: blue;
+    width:100px;
+    height:100px;
+    background-color:red;
+  `
+);
+
+export { StatHeader };
